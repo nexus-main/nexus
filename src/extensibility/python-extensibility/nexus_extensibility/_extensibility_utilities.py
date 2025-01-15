9c1f69cc567ff4507cf1ba1ecc3a9c1f299db55c
Vincent Wilms <git@m1.apollo3zehn.net>
2025-01-15 21:04:44 +0100

    Revert memory type annotations

--- src/extensibility/python-extensibility/nexus_extensibility/_extensibility_utilities.py
+++ src/extensibility/python-extensibility/nexus_extensibility/_extensibility_utilities.py
@@ -8,7 +8,7 @@ from ._data_model import Representation
 class ExtensibilityUtilities(ABC):
 
     @staticmethod
-    def create_buffers(representation: Representation, begin: datetime, end: datetime) -> Tuple[memoryview[float], memoryview[bytes]]:
+    def create_buffers(representation: Representation, begin: datetime, end: datetime) -> Tuple[memoryview, memoryview]:
         element_count = ExtensibilityUtilities._calculate_element_count(begin, end, representation.sample_period)
 
         data = bytearray(element_count * representation.element_size)
