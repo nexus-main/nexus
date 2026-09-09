8e78564557c16cac90a260aa903fa0cf697d0867
Vincent <vincent@anomaly.ai>
2026-09-09 13:00:46 +0000

    Clean up

--- tests/Nexus.UI.Tests/NexusDemoClientTests.cs
+++ tests/Nexus.UI.Tests/NexusDemoClientTests.cs
@@ -30,35 +30,4 @@ public class NexusDemoClientTests
         Assert.Equal("A description for the temperature resource.", response.Description);
         Assert.Equal("temperature", response.CatalogItem.Resource.Id);
     }
-
-    [Fact]
-    public async Task NonBufferedLoadAsyncThrows()
-    {
-        var client = new NexusDemoClient();
-        var begin = DateTime.UnixEpoch;
-
-        await Assert.ThrowsAsync<NotImplementedException>(() =>
-            client.LoadAsync<float>(begin, begin.AddMinutes(2), ["/SAMPLE/LOCAL/temperature/1_min"]));
-    }
-
-    [Fact]
-    public async Task LoadAsyncSupportsChunkAwareProvider()
-    {
-        var client = new NexusDemoClient();
-        var begin = DateTime.UnixEpoch;
-        var calls = new List<(int ChunkLength, long RemainingLength)>();
-
-        var result = await client.LoadAsync<float>(
-            begin,
-            begin.AddMinutes(2),
-            ["/SAMPLE/LOCAL/temperature/1_min"],
-            (_, chunkLength, remainingLength) =>
-            {
-                calls.Add((chunkLength, remainingLength));
-                return new float[chunkLength];
-            });
-
-        Assert.Equal((2, 2L), Assert.Single(calls));
-        Assert.Equal("temperature", Assert.Single(result).Value.Name);
-    }
 }
